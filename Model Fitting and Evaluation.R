60f7e354ffce1d63369b704b60e677ffcb8b24e4
James Smith <james.smith@unsw.edu.au>
2024-01-29 13:30:23 +1100

    Added script

---
+++ Model Fitting and Evaluation.R
@@ -0,0 +1,13 @@
+##################################################################
+###   Fitting, Predicting, Evaluating Joint and Stacked SDMs   ###  
+###    - As used for the Ocean Prawn Trawl observer data       ###
+###    - Publication: xxx                                      ###
+###    - James A. Smith Feb 2024                               ###
+##################################################################
+
+## This code is example code only and will not replicate results as published
+## because the observer data we used is confidential. But this code provides
+## the model structures and the fundamentals to repeat similar analyses.
+
+
+## Load packages
